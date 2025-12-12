f7836e9f037205fef1a7a11390bfe0fa1353419b
abreeling02-boop <abreeling02@gmail.com>
2025-12-12 13:57:43 -0600

    logic skeleton

--- logic.py
+++ logic.py
@@ -1,4 +1,52 @@
 from PyQt6.QtWidgets import *
 from gui import *
+from gui import Ui_Grade_Average
+import csv
 
-class Logic()
+class Student:
+    def __init__(self, name, scores):
+        pass
+
+    def best_score(self):
+        pass
+
+    def attempts(self):
+        pass
+
+
+class Logic(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+    def submit_clicked(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_attempts(self):
+        pass
+
+    def get_scores(self, attempts):
+        pass
+
+    def hide_scores(self):
+        pass
+
+    def show_scores(self, attempts):
+        pass
+
+    def clear_inputs(self):
+        pass
+
+    def clear_score_feedback(self):
+        pass
+
+    def set_status(self, message):
+        pass
+
+    def show_error(self, message):
+        pass
+
+    def save_to_csv(self):
+        pass
